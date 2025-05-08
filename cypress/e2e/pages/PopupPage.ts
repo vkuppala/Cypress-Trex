3b53726d49c874d66f1b0fdf64f51ef7a5a5181b
vkuppala <vkuppala@trex.com>
2025-05-08 11:42:07 -0400

    Update PopupPage.ts

--- cypress/e2e/pages/PopupPage.ts
+++ cypress/e2e/pages/PopupPage.ts
@@ -1,12 +1,1 @@
-export class PopupPage
-{
-
-    private readonly popupCloseButton = '.cmp-popup-modal__form-close button.close'
-
-    clickPopUpCloseButton()
-    {
-        cy.get(this.popupCloseButton).click()
-        cy.log(`Close the popup displayed`)
-    }
-
-}
+tes
